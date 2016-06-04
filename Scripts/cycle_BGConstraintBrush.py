69ec6aa60243bca400f02f43dcd7e4710744acfb
Tilapiatsu <tilapiatsu@hotmail.fr>
2016-06-05 00:36:58 +0200

    + Add shortcut to toggle symetry : ctrl-shift-x + Add shortcut to toggle lasso style : ctrl-alt-shift-a + Add shortcut to toggle partial selection : ctrl-alt-shift-z + Add shortcut to toggle between BGConstraintSmooth and BGConstraintSlide : Shift-lmb

---
+++ Scripts/cycle_BGConstraintBrush.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+import lx
+
+# You can add any tools you want to cycle through to this list.
+# The first tool in the list will be the default if none of them are active.
+# Or the last in the list if scrolling backwards.
+tools = ('SmoothBrushBGCons', 'BGConstraintSlideBrush')
+
+num_tools = len (tools)
+
+backwards = False
+args = lx.args ()
+if len (args) > 0:
+    backwards = (args[0].lower() == 'prev')
+
+active_tool = -1
+
+for tool_idx in xrange (num_tools):
+    if lx.test ('tool.set %s on' % tools[tool_idx]):
+        active_tool = tool_idx
+        break
+
+next_tool = active_tool
+
+if backwards:
+    next_tool = (active_tool - 1) % num_tools
+else:
+    next_tool = (active_tool + 1) % num_tools
+
+try:
+    lx.eval ('!!tool.set %s on' % tools[next_tool])
+except:
+    pass
