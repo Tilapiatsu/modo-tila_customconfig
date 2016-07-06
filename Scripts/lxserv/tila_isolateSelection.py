706d8a5bc53c5eb7f0379f0b940976c6449bcb13
Tilapiatsu <tilapiatsu@hotmail.fr>
2016-07-06 16:25:45 +0200

    * IsolateSelection now work in component mode. It work in Polygon mode. In edge or vertex, it automaticly convert the selection to adjacent polygon and isolate them

--- Scripts/lxserv/tila_isolateSelection.py
+++ Scripts/lxserv/tila_isolateSelection.py
@@ -43,17 +43,73 @@ class CmdTila_isolateSelection(lxu.command.BasicCommand):
 
         self.scn.select(self.selection)
 
+    def isPolygonSelected(self):
+        polygonIsSelected = False
+
+        for item in self.selection:
+            if len(item.geometry.polygons.selected) < 1:
+                polygonIsSelected = polygonIsSelected or False
+            else:
+                polygonIsSelected = polygonIsSelected or True
+
+        return polygonIsSelected
+
+    def isEdgeSelected(self):
+        edgeIsSelected = False
+
+        for item in self.selection:
+            if len(item.geometry.edges.selected) < 1:
+                edgeIsSelected = edgeIsSelected or False
+            else:
+                edgeIsSelected = edgeIsSelected or True
+
+        return edgeIsSelected
+
+    def isVertexSelected(self):
+        vertexIsSelected = False
+
+        for item in self.selection:
+            if len(item.geometry.vertices.selected) < 1:
+                vertexIsSelected = vertexIsSelected or False
+            else:
+                vertexIsSelected = vertexIsSelected or True
+
+        return vertexIsSelected
+
     def basic_Execute(self, msg, flags):
         if len(self.selection) == 0:
             self.scn.select(self.scn.items())
 
             self.isolateSelection()
         else:
-            lx.eval('hide.unsel')
+            if lx.eval('select.typeFrom item;pivot;center;edge;polygon;vertex;ptag ?'):
+                lx.eval('hide.unsel')
+
+            elif lx.eval('select.typeFrom polygon;edge;vertex;item;pivot;center;ptag ?'):
+                if self.isPolygonSelected():
+                    lx.eval('hide.unsel')
+                else:
+                    lx.eval('unhide')
+
+            elif lx.eval('select.typeFrom edge;vertex;polygon;item;pivot;center;ptag ?'):
+                if self.isEdgeSelected():
+                    lx.eval('select.expand')
+                    lx.eval('select.convert polygon')
+                    lx.eval('hide.unsel')
+                else:
+                    lx.eval('unhide')
+
+            elif lx.eval('select.typeFrom vertex;edge;polygon;item;pivot;center;ptag ?'):
+                if self.isVertexSelected():
+                    lx.eval('select.expand')
+                    lx.eval('select.expand')
+                    lx.eval('select.convert polygon')
+                    lx.eval('hide.unsel')
+                else:
+                    lx.eval('unhide')
 
     def cmd_Query(self, index, vaQuery):
         lx.notimpl()
-
 
 lx.bless(CmdTila_isolateSelection, "tila.isolateSelection")
 
