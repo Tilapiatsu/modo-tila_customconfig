f23767c4b708a644b57b3c771c44ffdde5bcd832
Tilapiatsu <tilapiatsu@hotmail.fr>
2017-02-04 22:07:17 +0100

    * Try to pre-Linearize the edge with the native modo tool

--- Scripts/LinearizeUv.py
+++ Scripts/LinearizeUv.py
@@ -1,14 +1,25 @@
 #python
 import lx
+lx.eval('select.convert edge')
+lx.eval('tool.viewType uv')
+lx.eval('tool.set xfrm.linearAlign on')
+lx.eval('tool.setAttr xfrm.linearAlign weight 1.0 ')
+lx.eval('tool.setAttr xfrm.linearAlign edgeLength 0.0')
+lx.eval('tool.doApply') 
 
-if lx.eval('select.typeFrom edge false'):
-	lx.eval('select.convert edge')
+
+lx.eval('tool.attr xfrm.linearAlign uniform true')
+lx.eval('tool.attr xfrm.linearAlign length false')
+lx.eval('tool.doApply')
+lx.eval('tool.set xfrm.linearAlign off')
+
 
 lx.eval('tool.set ffr.uvAlign on')
+lx.eval('tool.reset')
 
 lx.eval('tool.attr ffr.uvAlign axis endpoints')
 lx.eval('tool.attr ffr.uvAlign align average')
 lx.eval('tool.attr ffr.uvAlign spacing relative3d')
 lx.eval('tool.attr ffr.uvAlign length unchanged')
 lx.eval('tool.doApply')
-lx.eval('select.nextMode') 
+lx.eval('tool.set ffr.uvAlign off')
