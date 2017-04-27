b24c44f36c12618b7558c42702a972415b52f4eb
UBISOFT-ORG\lboucher <tilapiatsu@hotmail.fr>
2017-04-27 16:24:48 +0200

    + add a script to unparent and rename obj imported mesh based on his parent name.

---
+++ Scripts/UnparentAndRename.py
@@ -0,0 +1,16 @@
+import modo
+import lx
+
+scn = modo.Scene()
+
+selection = scn.selected
+
+for o in selection:
+	if o.type == 'groupLocator':
+		newName = o.name
+		o.name += '_new'
+		for c in o.children():
+			c.setParent()
+			c.name = newName
+		scn.select(o)
+		lx.eval('delete')	
