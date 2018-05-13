d1202b6f8ac5ee4ed001f10092d1555d54c800cd
Tilapiatsu <tilapiatsu@hotmail.fr>
2018-05-13 22:17:45 +0200

    + add Batch import OBJ, still rough, need to add a dialog box and integrate it into a contextual menu

---
+++ Scripts/BatchImportObj.py
@@ -0,0 +1,30 @@
+#python
+import modo, lx
+import os, sys
+
+rootPath
+
+lx.eval('dialog.setup dir')
+lx.eval('dialog.title "Open Path"')
+lx.eval('dialog.msg "Select path to process."')
+lx.eval('dialog.result "%s"' % rootPath)
+
+try:  # output folder dialog
+	lx.eval('dialog.open')
+except:
+	sys.exit()
+else:
+	root = lx.eval1('dialog.result ?')
+
+	for dirname, dirnames, filenames in os.walk(root):
+		
+		for f in filenames:
+			filepath = os.path.join(dirname, f)
+			ext = os.path.splitext(filepath)[1]
+			if ext.lower() == '.obj':
+				lx.out('importing {}'.format(f))
+				lx.eval('!!scene.open "{}" import'.format(filepath))
+			else:
+				pass
+		else:
+			lx.out('No file of type "obj" found')
