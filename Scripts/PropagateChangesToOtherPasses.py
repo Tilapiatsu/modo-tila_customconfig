d73c78ea171fd94c4fd1674a35420fd5d89e66d1
UBISOFT-ORG\lboucher <tilapiatsu@hotmail.fr>
2018-05-28 16:27:17 +0200

    + add a script to propagade changes made to some channels to all other passes of the renderPassGroup

---
+++ Scripts/PropagateChangesToOtherPasses.py
@@ -0,0 +1,41 @@
+# python
+
+import modo, lx
+
+scn = modo.Scene()
+
+selection = scn.selected
+
+rpg = None
+passe = None
+obj = None
+
+
+for o in selection:
+	if o.type == 'render':
+		rpg = o
+		continue
+	elif o.type == 'actionclip':
+		passe=o
+		continue
+	else:
+		obj = o
+		
+
+print rpg, passe, obj
+	
+for p in rpg.items:
+	
+	if p.type != 'actionclip':
+		continue
+	
+	if p.name == passe.name:
+		continue
+		
+	p.active = True
+	
+	lx.eval('channel.paste')
+	lx.eval('edit.apply')
+	
+	p.active = False
+	
